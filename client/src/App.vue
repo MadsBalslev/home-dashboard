99bbaa938ffea108db2235d0ee5aeff7840e5581
Mads Balslev <madspbalslev@gmail.com>
2021-01-11 22:30:00 +0100

    Intialize client

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Weather />
+</template>
+
+<script>
+import Weather from '@/components/weather.vue';
+
+export default {
+  components: {
+    Weather,
+  }
+}
+</script>
+
+<style lang="scss">
+html {
+  background-color: #0E131F;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #FDFFF7;
+}
+</style>
