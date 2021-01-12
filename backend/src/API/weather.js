2763395ffa1667b8d15722512de8234a03f26038
Mads Balslev <madspbalslev@gmail.com>
2021-01-12 12:17:22 +0100

    Change to OpenWeatherMap API

--- backend/src/API/weather.js
+++ backend/src/API/weather.js
@@ -1,7 +1,20 @@
 const fetch = require('node-fetch');
 
 const getWeather = async () => {
-  const response = await fetch('https://vejr.eu/api.php?location=aalborg&degree=C');
+  const baseUrl = 'https://api.openweathermap.org/data/2.5/onecall';
+  const paramaters = {
+    appid: process.env.WEATHER_API_KEY,
+    lat: '57.052631',
+    lon: '9.902810',
+    exclude: 'minutely,hourly',
+    units: 'metric',
+    lang: 'da'
+  }
+
+  const url = new URL(baseUrl);
+  url.search = new URLSearchParams(paramaters);
+
+  const response = await fetch(url);
   const json = await response.json();
 
   return json;
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 
 const news = require('./routes/news');
 const weather = require('./routes/weather')
